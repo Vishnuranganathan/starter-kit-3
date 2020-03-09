db712c42ef1753c0d4d6b8795664b191972f35c2
Pragyat Singh <Pragyat95@gmail.com>
2020-03-09 16:18:16 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_7B1NbYzn2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2p5j0862dk93c87a4gjmpp4c7r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
